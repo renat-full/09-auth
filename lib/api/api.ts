ebc11cc68871c421305f34b0449d6070659f12ba
Renat Moroz <bs4123555@gmail.com>
2025-11-13 01:02:11 +0200

    fix-2

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,10 +1,6 @@
 import axios from 'axios';
 
-// const baseURL = process.env.NEXT_PUBLIC_API_URL + '/api';
-
-const baseURL = process.env.NEXT_PUBLIC_API_URL
-  ? process.env.NEXT_PUBLIC_API_URL + '/api'
-  : '/api';
+const baseURL = process.env.NEXT_PUBLIC_API_URL + '/api';
 
 export const NextServer = axios.create({
   baseURL: baseURL,
